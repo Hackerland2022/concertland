5bf9eda35e875b67b4da97ee8f8fe54b08e1222b
franceneee <57038559+franceneee@users.noreply.github.com>
2022-07-03 19:29:10 +0800

    Update cheering.js

--- Cheering/cheering.js
+++ Cheering/cheering.js
@@ -2,65 +2,66 @@ window.onload = function () {
     const audienceCheer = document.getElementById("audienceCheer");
     const scream = document.getElementById("scream");
     const startBtn = document.getElementById("startBtn");
-    const trackContainer = document.querySelector('.track-container');
-    const keypress = document.querySelectorAll('.keypress');
+    const trackContainer = document.querySelector(".track-container");
+    const keypress = document.querySelectorAll(".keypress");
     const game = document.getElementById("game");
     const stats = document.getElementById("stats");
-    const comboText = document.querySelector('.hit__combo');
+    const comboText = document.querySelector(".hit__combo");
     const scoreCount = document.getElementById("score_count");
     const endGame = document.getElementById("endGame");
+    var id = null;
 
     var isHolding = {
-      a: false,
-      d: false,
+        a: false,
+        d: false,
     };
-    
+
     var hits = { good: 0, bad: 0, miss: 0 };
-    
+
     var a = {
-        color: 'rgba(28, 121, 228, 1)',
+        color: "rgba(28, 121, 228, 1)",
         next: 0,
         notes: [
-            { duration: 6, delay: 4.2 },
-            { duration: 6, delay: 12.2 },
-            { duration: 6, delay: 20.4 },
-            { duration: 6, delay: 28.1 },
-            { duration: 6, delay: 35.5 },
-            { duration: 6, delay: 39.5 },
-            { duration: 6, delay: 43.5 },
-            { duration: 6, delay: 47.4 },
-            { duration: 6, delay: 49.5 },
+            { duration: 4, delay: 4.2 },
+            { duration: 4, delay: 12.2 },
+            { duration: 4, delay: 20.4 },
+            { duration: 4, delay: 28.1 },
+            { duration: 4, delay: 35.5 },
+            { duration: 4, delay: 39.5 },
+            { duration: 4, delay: 43.5 },
+            { duration: 4, delay: 47.4 },
+            { duration: 4, delay: 49.5 },
         ],
     };
-    
+
     var d = {
-        color: 'rgba(28, 121, 228, 1)',
+        color: "rgba(28, 121, 228, 1)",
         next: 0,
         notes: [
-            { duration: 6, delay: 2.2 },
-            { duration: 6, delay: 6.2 },
-            { duration: 6, delay: 10.2 },
-            { duration: 6, delay: 18.5 },
-            { duration: 6, delay: 22.4 },
-            { duration: 6, delay: 26.2 },
-            { duration: 6, delay: 29.9 },
-            { duration: 6, delay: 33.6 },
-            { duration: 6, delay: 37.3 },
-            { duration: 6, delay: 39.5 },
-            { duration: 6, delay: 41.3 },
-            { duration: 6, delay: 47.4 },
+            { duration: 4, delay: 2.2 },
+            { duration: 4, delay: 6.2 },
+            { duration: 4, delay: 10.2 },
+            { duration: 4, delay: 18.5 },
+            { duration: 4, delay: 22.4 },
+            { duration: 4, delay: 26.2 },
+            { duration: 4, delay: 29.9 },
+            { duration: 4, delay: 33.6 },
+            { duration: 4, delay: 37.3 },
+            { duration: 4, delay: 39.5 },
+            { duration: 4, delay: 41.3 },
+            { duration: 4, delay: 47.4 },
         ],
     };
-    
+
     var song = {
-      duration: 56,
-      sheet: [a, d],
+        duration: 56,
+        sheet: [a, d],
     };
-    
+
     var multiplier = {
-      good: 1,
-      bad: 0.5,
-      miss: 0,
+        good: 1,
+        bad: 0.5,
+        miss: 0,
     };
 
     var isPlaying = false;
@@ -72,33 +73,33 @@ window.onload = function () {
     var tracks;
 
     var initialiseNotes = function () {
-      var noteElement;
-      var trackElement;
-    
-      while (trackContainer.hasChildNodes()) {
-        trackContainer.removeChild(trackContainer.lastChild);
-      }
-    
-      song.sheet.forEach(function (key, index) {
-        trackElement = document.createElement('div');
-        trackElement.classList.add('track');
-    
-        key.notes.forEach(function (note) {
-          noteElement = document.createElement('div');
-          noteElement.classList.add('note');
-          noteElement.classList.add('note--' + index);
-          noteElement.style.backgroundColor = key.color;
-          noteElement.style.animationName = animation;
-          noteElement.style.animationTimingFunction = 'linear';
-          noteElement.style.animationDuration = note.duration + 's';
-          noteElement.style.animationDelay = note.delay+ 's';
-          noteElement.style.animationPlayState = 'paused';
-          trackElement.appendChild(noteElement);
+        var noteElement;
+        var trackElement;
+
+        while (trackContainer.hasChildNodes()) {
+            trackContainer.removeChild(trackContainer.lastChild);
+        }
+
+        song.sheet.forEach(function (key, index) {
+            trackElement = document.createElement("div");
+            trackElement.classList.add("track");
+
+            key.notes.forEach(function (note) {
+                noteElement = document.createElement("div");
+                noteElement.classList.add("note");
+                noteElement.classList.add("note--" + index);
+                noteElement.style.backgroundColor = key.color;
+                noteElement.style.animationName = animation;
+                noteElement.style.animationTimingFunction = "linear";
+                noteElement.style.animationDuration = note.duration + "s";
+                noteElement.style.animationDelay = note.delay + "s";
+                noteElement.style.animationPlayState = "paused";
+                trackElement.appendChild(noteElement);
+            });
+
+            trackContainer.appendChild(trackElement);
+            tracks = document.querySelectorAll(".track");
         });
-    
-        trackContainer.appendChild(trackElement);
-        tracks = document.querySelectorAll('.track');
-      });
     };
 
     document.addEventListener("keydown", function (event) {
@@ -144,13 +145,10 @@ window.onload = function () {
         stats.style.display = "block";
         endGame.style.display = "none";
         startTimer(song.duration);
-        document.getElementById('song').play();
+        document.getElementById("song").play();
         initialiseNotes();
-        document.querySelectorAll('.note').forEach(function (note) {
-          note.style.animationPlayState = 'running';
-          note.addEventListener(true, function() {
-            if(note.style.top >= "70vh") audienceCheer.play();
-          })
+        document.querySelectorAll(".note").forEach(function (note) {
+            note.style.animationPlayState = "running";
         });
     }
 
@@ -176,75 +174,77 @@ window.onload = function () {
     };
 
     var updateHits = function (judgement) {
-      hits[judgement]++;
+        hits[judgement]++;
     };
 
     var calculateScore = function (judgement) {
-      score += 1000 * multiplier[judgement];
-      scoreCount.innerHTML = `Score: ${score}`;
+        score += 1000 * multiplier[judgement];
+        scoreCount.innerHTML = `Score: ${score}`;
     };
 
     var updateCombo = function (judgement) {
-      if (judgement === 'miss') {
-        combo = 0;
-      } else {
-        ++combo;
-      }
-      
-      comboText.innerHTML = `Combo: ${combo}`;
+        if (judgement === "miss") {
+            combo = 0;
+        } else {
+            ++combo;
+        }
+
+        comboText.innerHTML = `Combo: ${combo}`;
     };
 
     var updateMaxCombo = function () {
-      maxCombo = maxCombo > combo ? maxCombo : combo;
+        maxCombo = maxCombo > combo ? maxCombo : combo;
     };
 
     var getHitJudgement = function (accuracy) {
-      if (accuracy < 0.2) {
-        return 'good';
-      } else if (accuracy < 0.3) {
-        return 'bad';
-      } else {
-        return 'miss';
-      }
+        if (accuracy < 0.2) {
+            audienceCheer.play();
+            return "good";
+        } else if (accuracy < 0.3) {
+            audienceCheer.play();
+            return "bad";
+        } else {
+            return "miss";
+        }
     };
 
     var removeNoteFromTrack = function (parent, child) {
-      parent.removeChild(child);
+        parent.removeChild(child);
     };
 
     var updateNext = function (index) {
-      song.sheet[index].next++;
+        song.sheet[index].next++;
     };
 
     var startTimer = function (duration) {
-      var display = document.querySelector('.summary__timer');
-      var timer = duration;
-      var minutes;
-      var seconds;
-    
-      display.style.display = 'block';
-      display.style.opacity = 1;
-    
-      var songDurationInterval = setInterval(function () {
-        minutes = Math.floor(timer / 60);
-        seconds = timer % 60;
-        minutes = minutes < 10 ? '0' + minutes : minutes;
-        seconds = seconds < 10 ? '0' + seconds : seconds;
-        display.innerHTML = minutes + ':' + seconds;
-    
-        if (--timer < 0) {
-          clearInterval(songDurationInterval);
-          showResult();
-          comboText.style.transition = 'all 1s';
-          comboText.style.opacity = 0;
-        }
-      }, 1000);
+        var display = document.querySelector(".summary__timer");
+        var timer = duration;
+        var minutes;
+        var seconds;
+
+        display.style.display = "block";
+        display.style.opacity = 1;
+
+        var songDurationInterval = setInterval(function () {
+            minutes = Math.floor(timer / 60);
+            seconds = timer % 60;
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+            display.innerHTML = minutes + ":" + seconds;
+
+            if (--timer < 0) {
+                clearInterval(songDurationInterval);
+                showResult();
+                comboText.style.transition = "all 1s";
+                comboText.style.opacity = 0;
+            }
+        }, 1000);
     };
 
     function showResult() {
-      document.getElementById("endGame").style.display = "flex";
-      document.getElementById("finalScore").innerHTML = `Score: ${score}`;
-      game.style.display = "none";
-      stats.style.display = "none";
+        document.getElementById("endGame").style.display = "flex";
+        document.getElementById("finalScore").innerHTML = `Score: ${score}`;
+        game.style.display = "none";
+        stats.style.display = "none";
     }
 };
